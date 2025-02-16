487d10718c82dcb24a54ddd13107ae65e89d4da4
azabiyaliev <aza.biyaliev@gmail.com>
2025-02-16 18:27:19 +0600

    [HW#94] Реализована модель пользователя без определения ролей, добавлены эндпоинты: регистрация, логин и логаут.

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,12 @@ import { AlbumsController } from './albums/albums.controller';
 import { Album, AlbumSchema } from './schemas/album.schema';
 import { TracksController } from './tracks/tracks.controller';
 import { Track, TrackSchema } from './schemas/track.schema';
+import { User, UserSchema } from './schemas/user.schema';
+import { UsersController } from './users/users.controller';
+import { AuthService } from './auth/auth.service';
+import { LocalStrategy } from './local.strategy';
+import { PassportModule } from '@nestjs/passport';
+import { TokenAuthGuard } from './token-auth/token-auth.guard';
 
 @Module({
   imports: [
@@ -16,9 +22,17 @@ import { Track, TrackSchema } from './schemas/track.schema';
       { name: Artist.name, schema: ArtistSchema },
       { name: Album.name, schema: AlbumSchema },
       { name: Track.name, schema: TrackSchema },
+      { name: User.name, schema: UserSchema },
     ]),
+    PassportModule,
   ],
-  controllers: [AppController, ArtistsController, AlbumsController, TracksController],
-  providers: [AppService],
+  controllers: [
+    AppController,
+    ArtistsController,
+    AlbumsController,
+    TracksController,
+    UsersController,
+  ],
+  providers: [AppService, AuthService, LocalStrategy, TokenAuthGuard],
 })
 export class AppModule {}
---
+++ src/users/users.controller.ts
@@ -0,0 +1,37 @@
+import { Body, Controller, Delete, Post, Req, UseGuards } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from '../schemas/user.schema';
+import { RegisterUserDto } from './register-user.dto';
+import { Request } from 'express';
+import { AuthGuard } from '@nestjs/passport';
+import { TokenAuthGuard } from '../token-auth/token-auth.guard';
+
+@Controller('users')
+export class UsersController {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  @Post()
+  registerUser(@Body() registerUserDto: RegisterUserDto) {
+    const user: UserDocument = new this.userModel({
+      email: registerUserDto.email,
+      password: registerUserDto.password,
+      displayName: registerUserDto.displayName,
+    });
+
+    user.generateToken();
+    return user.save();
+  }
+
+  @UseGuards(AuthGuard('local'))
+  @Post('sessions')
+  login(@Req() req: Request<{ user: User }>) {
+    return { user: req.user, message: 'Login successfully' };
+  }
+
+  @UseGuards(TokenAuthGuard)
+  @Delete('sessions')
+  logout(@Req() req: Request<{ user: User }>) {
+    return { user: req.user, message: 'Logout successfully' };
+  }
+}
